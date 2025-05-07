d168f28242169ecf2fb6f5e3b0b10f91e1062ac1
subcaptaintsubasa <tsubasa.hsty@gmail.com>
2025-05-07 15:59:03 +0900

    Update admin.script.js

--- admin/admin.script.js
+++ admin/admin.script.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
         storageBucket: "itemsearchtooleditor.appspot.com",
         messagingSenderId: "243156973544",
         appId: "1:243156973544:web:ffdc31134a35354b6dd65d"
+        measurementId: "G-8EHP9MGJ4M"
     };
     // ▲▲▲ Firebaseプロジェクトの設定情報 ▲▲▲
 
