13fbaa0ccaad269812ff6386394c01721738775e
subcaptaintsubasa <tsubasa.hsty@gmail.com>
2025-05-16 08:45:42 +0900

    Create admin-config.js

---
+++ admin/admin-config.js
@@ -0,0 +1,13 @@
+// admin-config.js
+// Firebase設定をこのファイルに分離します。
+export const firebaseConfig = {
+  apiKey: "AIzaSyBxrE-9E46dplHTuEBmmcJWQRU1vLgAGAU",
+  authDomain: "itemsearchtooleditor.firebaseapp.com",
+  projectId: "itemsearchtooleditor",
+  storageBucket: "itemsearchtooleditor.appspot.com",
+  messagingSenderId: "243156973544",
+  appId: "1:243156973544:web:ffdc31134a35354b6dd65d",
+  measurementId: "G-8EHP9MGJ4M"
+};
+
+export const IMAGE_UPLOAD_WORKER_URL = 'https://denpa-item-uploader.tsubasa-hsty-f58.workers.dev';
